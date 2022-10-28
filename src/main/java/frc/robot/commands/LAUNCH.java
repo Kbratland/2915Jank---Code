9bfe82358df54b868c174f176c49e8d0fc2abb78
Kbratland <116665821+Kbratland@users.noreply.github.com>
2022-10-28 10:02:17 -0700

    4

---
+++ src/main/java/frc/robot/commands/LAUNCH.java
@@ -0,0 +1,9 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ShooterSystem;
+
+public class LAUNCH extends CommandBase {
+    public ShooterSystem shooterSystem;
+    long duration = 0;
+}
---
+++ src/main/java/frc/robot/subsystems/ShooterSystem.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystems;
+
+import com.fasterxml.jackson.databind.jsontype.impl.SubTypeValidator;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ShooterSystem extends SubsystemBase {
+    private final PWMSparkMax shooterNeo = new PWMSparkMax(9);
+    Encoder encoder = new Encoder(0, 1);
+
+    public void resetEncoders() {
+        encoder.reset();
+    }
+
+}
