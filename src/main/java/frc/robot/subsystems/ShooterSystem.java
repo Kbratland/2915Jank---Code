60b2cde9014025e55053e3d8791254efd80583ad
Kbratland <116665821+Kbratland@users.noreply.github.com>
2022-11-01 14:03:58 -0700

    e

--- src/main/java/frc/robot/subsystems/ShooterSystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSystem.java
@@ -1,13 +1,13 @@
 package frc.robot.subsystems;
 
 // import edu.wpi.first.wpilibj.Encoder;
-// import com.revrobotics.CANSparkMax;
-//import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DriveConstants;
 
 public class ShooterSystem extends SubsystemBase {
-    private final CANSparkMax neoShooter = new CANSparkMax(DriveConstants.kShooterPort);
+    private final CANSparkMax neoShooter = new CANSparkMax(DriveConstants.kShooterPort, true);
     // Encoder encoder = new Encoder(0, 1);
 
     // public void resetEncoders() {
