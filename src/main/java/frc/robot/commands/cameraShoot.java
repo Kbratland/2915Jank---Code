db5af22e4a27523386cd46ba95e625c6fc2c9193
Kbratland <116665821+Kbratland@users.noreply.github.com>
2022-11-17 09:48:27 -0800

    e

---
+++ src/main/java/frc/robot/commands/cameraShoot.java
@@ -0,0 +1,52 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Camera;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.ShooterSystem;
+
+public class cameraShoot extends CommandBase{
+    DriveSubsystem driveSubsystem;
+    ShooterSystem shooterSystem;
+    Camera camera;
+    double targetAngle = 0;
+    long timerStart;
+    boolean started = false;
+    long duration = 1500;
+    public cameraShoot(DriveSubsystem driveSubsystem, ShooterSystem shooterSystem, Camera camera){
+        this.driveSubsystem = driveSubsystem;
+        this.shooterSystem = shooterSystem;
+        this.camera = camera;
+    }
+    @Override
+    public void initialize(){
+        targetAngle = camera.getTargetYaw();
+    }
+    public void execute(){
+        if(driveSubsystem.getHeading() < targetAngle - 0.3){
+            driveSubsystem.arcadeDrive(0, 0.45);
+            System.out.println("turning pos");
+        }
+        else if(driveSubsystem.getHeading() > targetAngle + 0.3){
+            driveSubsystem.arcadeDrive(0, -0.45);
+            System.out.println("turning Neg");
+        }
+        else if (driveSubsystem.getHeading() > targetAngle - 0.3 && driveSubsystem.getHeading() < targetAngle + 0.3){
+            driveSubsystem.arcadeDrive(0, 0);
+            shooterSystem.shoot(1);
+            System.out.println("shooting");
+            if(!started){
+                timerStart = System.currentTimeMillis();
+                started = true;
+            }
+        }
+    }
+    public boolean isFinished(){
+        return (duration < System.currentTimeMillis() - timerStart);
+    }
+    public void end(boolean interupted){
+        shooterSystem.end();
+        driveSubsystem.arcadeDrive(0, 0);
+    }
+
+}
