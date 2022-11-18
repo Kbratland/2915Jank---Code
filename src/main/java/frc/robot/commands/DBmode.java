4da91277bd2d10afaa1019b3720935ed998dfc7c
Flamepupper <113953766+Flamepupper@users.noreply.github.com>
2022-11-18 12:21:11 -0800

    good

--- src/main/java/frc/robot/commands/DBmode.java
+++ src/main/java/frc/robot/commands/DBmode.java
@@ -10,6 +10,9 @@ public class DBmode extends CommandBase {
     double a = camera.getTargetArea();
         double x = camera.getTargetYaw();
         double tolx = 10;
+        double tolap = 10;
+        double tolan = 10;
+        boolean BatteringRam = false;
         double r = 0;
     DriveSubsystem driveSubsystem;
     public DBmode(Camera camera, DriveSubsystem driveSubsystem) {
@@ -19,64 +22,37 @@ public class DBmode extends CommandBase {
     public void initialize(){
       tolx = 10;
     }
-    //e
     public void execute(){
-        a = camera.getTargetArea();
-        x = camera.getTargetYaw();
-        System.out.println("depth " + a + ", Yaw " + x);
-        //driveSubsystem.arcadeDrive(0, yaw * -1);
-        if (x > -tolx && x < tolx){
-          driveSubsystem.arcadeDrive(0, 0);
-        }
+    System.out.println("depth " + a + ", Yaw " + x);
+    if (x < -tolx && tolx < x){
         if(x < -tolx){
-          r = 0;
-          if (x > -tolx && x > tolx ){
-            r = (x/10)*-1;
-          }
-          else {
-            r = -0.5;
-          }
+          r = -(x/10);
           driveSubsystem.arcadeDrive(0, r);
-        }
-        else if(x < 10 && x > 5){
-          r = 0;
-          if (x < 10 && x > 5){
-          r = (x/10); 
           }
-          else {
-            r = 0.5;
-          }
+        if(x > tolx){
+          r =  (x/10); 
           driveSubsystem.arcadeDrive(0, r);
-        }
-        if(a < 5){ 
-          //0=1 & 5 = 0.5
-          // 5 - 0, 0.5 - 1
-          // -5 to 0 +5
-          //a = (a+5)/10;
-          a = (a * -1) + 5;
-          a = a/5;
-          if (a < 0.5){
-          a = 0.5;
           }
+    }
+    if (BatteringRam && (a > tolap && a < tolan)){
+        if(a < tolan){ 
+          a = ((a * -1) + 5)/5;
           driveSubsystem.arcadeDrive(a , 0);
         }
-        else if (a > 20 && x < 5){
+          if (a < tolap){
+          a = 0.45;
+          driveSubsystem.arcadeDrive(a , 0);
+          }
+        }
           driveSubsystem.arcadeDrive(0, 0);
         }
-        else if (a > 20){
-          a = a/40;
-          // 30 - 20, 1 - 0.5
 
-          driveSubsystem.arcadeDrive(-a, 0);
-        }
-        else{
-        driveSubsystem.arcadeDrive(0, 0);
-        }
+public void end (boolean interupted){
+  driveSubsystem.arcadeDrive(0, 0);
     }
-    public void end (boolean interupted){
-      driveSubsystem.arcadeDrive(0, 0);
-    }
-    public boolean isFinished(){
-      return false;
-    }
+
+public boolean isFinished(){
+    return false;
+  }
+
 }
