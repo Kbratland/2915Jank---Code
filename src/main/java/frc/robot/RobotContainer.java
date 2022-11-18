c6c3b83d10a9521c25ced0b10063c985c73b5654
Kbratland <116665821+Kbratland@users.noreply.github.com>
2022-11-18 12:32:17 -0800

    e

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -21,7 +21,7 @@ import frc.robot.commands.AUTONOMOUSforwards;
 import frc.robot.commands.BARF;
 import frc.robot.commands.CameraPID;
 import frc.robot.commands.CameraTrack;
-// import frc.robot.commands.DBmode;
+import frc.robot.commands.DBmode;
 import frc.robot.commands.FONDLE;
 import frc.robot.commands.LAUNCH;
 import frc.robot.commands.cameraShoot;
@@ -61,7 +61,7 @@ public class RobotContainer {
   private final FONDLE fondle = new FONDLE(m_beltSubsystem, m_driverController);
   private final LAUNCH launch = new LAUNCH(m_shooterSystem, 1);
   private final CameraTrack cameraTrack = new CameraTrack(camera, m_robotDrive);
-//   private final DBmode dBmode = new DBmode(camera, m_robotDrive);
+  private final DBmode dBmode = new DBmode(camera, m_robotDrive);
   private final cameraShoot camerashoot = new cameraShoot(
     m_robotDrive,
     m_shooterSystem,
@@ -150,7 +150,7 @@ public class RobotContainer {
     button5.whenPressed(fondle);
     button6.cancelWhenPressed(fondle);
     button7.whileHeld(cameraTrack);
-    // button8.whileHeld(dBmode);
+    button8.whileHeld(dBmode);
     button9.whenPressed(new CameraPID(camera.getTargetYaw(), m_robotDrive));
     button10.whenPressed(camerashoot);
   }
